8638fa4fcebf0df519e537088424efc5234b24fd
jleblanc64 <dabadich@gmail.com>
2021-11-20 16:28:38 +0100

    LocalMySQL

---
+++ src/test/java/com/LocalMySQL.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.testcontainers.containers.MySQLContainer;
+
+public class LocalMySQL {
+
+    public static void main(String[] args) throws Exception {
+        MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0.23");
+        mysql.start();
+
+        System.out.println(mysql.getJdbcUrl());
+
+        Thread.sleep(100000000);
+    }
+}
