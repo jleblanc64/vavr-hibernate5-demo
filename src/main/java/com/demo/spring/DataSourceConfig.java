8e3b97704c2c2fc744f97f5685168fc6607eed99
charles.dabadie <charles.dabadie@sympheny.com>
2024-12-27 15:17:40 +0100

    move libCustom code to external lib

--- src/main/java/com/demo/spring/DataSourceConfig.java
+++ src/main/java/com/demo/spring/DataSourceConfig.java
@@ -2,21 +2,13 @@ package com.demo.spring;
 
 import com.zaxxer.hikari.HikariDataSource;
 import io.github.jleblanc64.libcustom.LibCustom;
-import io.github.jleblanc64.libcustom.custom.utils.FieldCustomType;
-import io.github.jleblanc64.libcustom.custom.utils.TypeImpl;
-import io.vavr.control.Option;
+import io.github.jleblanc64.libcustom.custom.hibernate.VavrHibernate5;
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
-import java.lang.reflect.Field;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.util.List;
-
-import static io.github.jleblanc64.libcustom.FieldMocked.getRefl;
 
 @Configuration
 public class DataSourceConfig {
@@ -30,82 +22,8 @@ public class DataSourceConfig {
     String password;
 
     @Bean
-    public DataSource getDataSource() throws ClassNotFoundException {
-        Class<?> listClass = io.vavr.collection.List.class;
-        Class<?> optionClass = Option.class;
-
-        var bagTypeClass = Class.forName("org.hibernate.type.BagType");
-        var setterFieldImplClass = Class.forName("org.hibernate.property.access.spi.SetterFieldImpl");
-        var getterFieldImplClass = Class.forName("org.hibernate.property.access.spi.GetterFieldImpl");
-
-
-        LibCustom.modifyReturn(Class.forName("org.hibernate.metamodel.internal.AttributeFactory$BaseAttributeMetadata"), "getJavaType", x -> {
-            var clazz = x.returned;
-            if (clazz == io.vavr.collection.List.class)
-                return List.class;
-
-            return LibCustom.ORIGINAL;
-        });
-
-        LibCustom.modifyArgWithSelf(setterFieldImplClass, "set", 1, argsSelf -> {
-            var args = argsSelf.args;
-            var value = args[1];
-            var self = argsSelf.self;
-            var field = (Field) getRefl(self, setterFieldImplClass.getDeclaredField("field"));
-
-            if (field.getType() == listClass)
-                return io.vavr.collection.List.ofAll((List) value);
-
-            if (field.getType() == optionClass && !(value instanceof Option))
-                return Option.of(value);
-
-            return LibCustom.ORIGINAL;
-        });
-
-        LibCustom.modifyReturn(getterFieldImplClass, "get", x -> {
-            var ret = x.returned;
-            if (ret instanceof Option)
-                return ((Option) ret).getOrNull();
-
-            return ret;
-        });
-
-        LibCustom.modifyArg(Class.forName("org.hibernate.annotations.common.reflection.java.JavaXProperty"), "create", 0, args -> {
-            var member = args[0];
-            if (member instanceof Field) {
-                var field = (Field) member;
-                if (!(field.getGenericType() instanceof ParameterizedType))
-                    return LibCustom.ORIGINAL;
-
-                var type = (ParameterizedType) field.getGenericType();
-                var typeRaw = type.getRawType();
-                var typeParam = type.getActualTypeArguments()[0];
-                if (typeRaw == listClass)
-                    return FieldCustomType.create(field, new TypeImpl(List.class, new Type[]{typeParam}, null));
-
-                if (typeRaw == optionClass)
-                    return FieldCustomType.create(field, new TypeImpl((Class<?>) typeParam, new Type[]{}, null));
-            }
-
-            return LibCustom.ORIGINAL;
-        });
-
-        LibCustom.modifyArg(Class.forName("org.hibernate.type.CollectionType"), "getElementsIterator", 0, args -> {
-            var collection = args[0];
-            if (collection instanceof io.vavr.collection.List)
-                return ((io.vavr.collection.List) collection).toJavaList();
-
-            return collection;
-        });
-
-        LibCustom.modifyArg(bagTypeClass, "wrap", 1, args -> {
-            var collection = args[1];
-            if (collection instanceof io.vavr.collection.List)
-                return ((io.vavr.collection.List) collection).toJavaList();
-
-            return collection;
-        });
-
+    public DataSource getDataSource() {
+        VavrHibernate5.override();
         LibCustom.load();
 
         // Hikari
