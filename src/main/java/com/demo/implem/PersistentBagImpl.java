04984988eb0755952f4ba01e2d5b0bb5c27fcc65
charles.dabadie <charles.dabadie@sympheny.com>
2025-02-24 14:59:58 +0100

    cleanup

--- src/main/java/com/demo/implem/PersistentBagImpl.java
+++ src/main/java/com/demo/implem/PersistentBagImpl.java
@@ -1,6 +1,6 @@
 package com.demo.implem;
 
-import io.github.jleblanc64.libcustom.custom.hibernate.duplicate.MyPersistentBag;
+import io.github.jleblanc64.hibernate5.hibernate.duplicate.MyPersistentBag;
 import io.vavr.PartialFunction;
 import io.vavr.collection.List;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
