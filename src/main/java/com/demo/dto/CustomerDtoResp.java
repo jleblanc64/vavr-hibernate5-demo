6cd8e2ef2e68fd18028097c0231802ca7e6fc5a8
charles.dabadie <charles.dabadie@sympheny.com>
2025-02-24 15:02:25 +0100

    cleanup

--- src/main/java/com/demo/dto/CustomerDtoResp.java
+++ src/main/java/com/demo/dto/CustomerDtoResp.java
@@ -7,8 +7,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import static io.github.jleblanc64.libcustom.functional.Functor.print;
-
 @Getter
 @Setter
 @NoArgsConstructor
